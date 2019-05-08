862221068c7a040308d355deca07016dea445c7b
jason <jasonkellogglee@gmail.com>
2019-05-08 07:16:58 -0700

    added summary

--- server.js
+++ server.js
@@ -15,8 +15,6 @@ var PORT = 3000;
 // Initialize Express
 var app = express();
 
-// Configure middleware
-
 // Use morgan logger for logging requests
 app.use(logger("dev"));
 // Parse request body as JSON
@@ -29,6 +27,7 @@ app.use(express.static("public"));
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
 mongoose.connect(MONGODB_URI);
+// mongoose.connect("mongodb://localhost/news-scrape", { useNewUrlParser: true });
 
 // Routes
 
@@ -39,16 +38,22 @@ app.get("/scrape", function(req, res) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
-    // Now, we grab every a within an ArticleCard__title, and do the following:
-    $("a.ArticleCard__title").each(function(i, element) {
+    // Now, we grab every a within an ArticleCard__details-container, and do the following:
+    $(".ArticleCard__details-container").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
-      // Add the text and href of every link, and save them as properties of the result object
+      // Add the title, description, and href of every link, and save them as properties of the result object
       result.title = $(this)
+        .children("a.ArticleCard__title")
         .text();
       result.link = $(this)
+        .children("a.ArticleCard__title")
         .attr("href");
+      result.summary = $(this)
+        .children("a.ArticleCard__description")
+        .text()
+        // .replace('"','');
 
       // Create a new Article using the `result` object built from scraping
       db.Article.create(result)
@@ -69,7 +74,7 @@ app.get("/scrape", function(req, res) {
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
-  // TODO: Finish the route so it grabs all of the articles
+  // Finish the route so it grabs all of the articles
   db.Article.find({})
   .then(function(dbArticle) {
     res.json(dbArticle);
@@ -81,8 +86,6 @@ app.get("/articles", function(req, res) {
 
 // Route for grabbing a specific Article by id, populate it with it's note
 app.get("/articles/:id", function(req, res) {
-  // TODO
-  // ====
   // Finish the route so it finds one article using the req.params.id,
   // and run the populate method with "note",
   // then responds with the article with the note included
